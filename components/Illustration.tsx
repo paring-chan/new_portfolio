5b2af787f7e0d2d4fc0fb68369297a98a7b20bdb
파랑이 <parangee9706@gmail.com>
2021-07-28 23:35:06 +0900

    finish illustration section

--- components/Illustration.tsx
+++ components/Illustration.tsx
@@ -9,13 +9,44 @@ const Illustration: React.FC<{
     }
 }> = ({ i }) => {
     return (
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                position: 'relative',
+            }}
+        >
+            <video
+                src={i.vid}
+                autoPlay
+                loop
+                style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100vw',
+                    height: '100vh',
+                    objectFit: 'cover',
+                    zIndex: 99,
+                }}
+            />
+            <div
+                style={{
+                    zIndex: 100,
+                    background: '#000',
+                    position: 'absolute',
+                    width: '100vw',
+                    height: '100vh',
+                    opacity: 0.3,
+                }}
+            />
             <img
                 src={i.img.src}
                 alt={i.title}
                 style={{
                     width: '100vmin',
                     height: '100vmin',
+                    zIndex: 101,
                 }}
             />
         </div>
