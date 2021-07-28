832be52005d56a1dd918ecd937214aa5739c7597
파랑이 <parangee9706@gmail.com>
2021-07-28 22:03:31 +0900

    hero section

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import Head from 'next/head'
+
+library.add(fab, fas)
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>STORM</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,44 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import styles from '../styles/Home.module.scss'
+import profile from '../assets/img/profile.png'
+
 export default function Home() {
     return (
         <div>
-            HOME
+            <div className={styles.hero}>
+                <div className={styles.content}>
+                    <img
+                        src={profile.src}
+                        width={200}
+                        style={{ borderRadius: '50%' }}
+                        alt="profile"
+                    />
+                    <h1>STORM</h1>
+                    <h2>I'm Illustrator & Developer</h2>
+                    <div className={styles.links}>
+                        <a href="tel:01054987416">
+                            <FontAwesomeIcon icon={['fas', 'phone']} />
+                        </a>
+                        <a href="mailto:me@stormdev.club">
+                            <FontAwesomeIcon icon={['fas', 'envelope']} />
+                        </a>
+                        <a
+                            target="_blank"
+                            href="https://github.com/uniquedevstorm"
+                        >
+                            <FontAwesomeIcon icon={['fab', 'github']} />
+                        </a>
+                    </div>
+                </div>
+                <div style={{ position: 'relative', height: 100 }}>
+                    <a href="#" className={styles.scrollButton}>
+                        <span />
+                        <span />
+                        <span />
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
