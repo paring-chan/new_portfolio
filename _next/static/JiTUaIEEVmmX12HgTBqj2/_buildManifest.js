4b1dd139cd2cf7fd05c4cfc888ee393e82d3c525
pikokr <pikokr@users.noreply.github.com>
2021-07-30 01:26:31 +0000

    deploy: 5fd4d7652b749fd498b50b6468f46702dc155bc3

---
+++ _next/static/JiTUaIEEVmmX12HgTBqj2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/801a6084-76ea09f1067c056a0864.js","static/css/e1353879e9fe963bcd77.css","static/chunks/pages/index-701f4a64e33e84d8aeae.js"],"/_error":["static/chunks/pages/_error-82a806cd39f8ab3dc3ac.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
